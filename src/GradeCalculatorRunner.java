8dbb15f4c76b78372441356eb9dc14b31454aa67
Rizwan <119430628+ImR10@users.noreply.github.com>
2025-02-09 05:16:09 -0500

    Add files via upload

---
+++ src/GradeCalculatorRunner.java
@@ -0,0 +1,48 @@
+/*
+ * This class is responsible for running the grade calculator.
+ */
+public class GradeCalculatorRunner {
+	public static void main(String[] args) {
+		
+		GradeCalculator gc = new GradeCalculator();
+		
+		gc.getScale();
+		gc.setScale();
+		gc.gradeToPass();
+		gc.getInfo();
+		gc.setCatInfo();
+		gc.currentGrade();
+		gc.getLetterGrade();
+		gc.printPass();
+
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
